a068e2c3f81d7e406d579bcac4436229fd903e31
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-09-21 13:01:21 +0200

    Improve caching and meteor installation

---
+++ install_meteor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# If the cached meteor directory exists, link its binary
+# Else install meteor.
+if [ -e ~/.meteor/meteor ]; then
+  ./symlink_cached_meteor_binary.sh
+else
+  echo "Meteor directory not found. Installing Meteor $METEOR_VERSION..."
+  curl "https://install.meteor.com?release=$METEOR_VERSION" | /bin/sh
+fi
+
+# FIXME: Do this temporarily while 1.7.1 can't be installed via curl
+# meteor update --release 1.7.1-beta.19
